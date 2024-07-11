5d9ebf5027dd1c73dc7cfa4885b27701e2286abd
akinpinkman <akinbilgin54@hotmail.com>
2024-07-11 10:27:46 +0300

    Add book reducer, actions, model, and app state interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { StoreModule } from '@ngrx/store';
+import { BookReducer } from './books/book.reducer';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, BookListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ book: BookReducer }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
