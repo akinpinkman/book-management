dd8701e30def3a12e3ca4dbbc82a2312b86aa04b
akinpinkman <akinbilgin54@hotmail.com>
2024-07-12 09:13:57 +0300

    Add AddBookSuccess and AddBookFailure actions to book reducer

--- src/app/books/book.reducer.ts
+++ src/app/books/book.reducer.ts
@@ -1,17 +1,28 @@
 import { createReducer, on } from '@ngrx/store';
-import { AddBook, RemoveBook } from './book.actions';
+import {
+  AddBook,
+  RemoveBook,
+  AddBookFailure,
+  AddBookSuccess,
+} from './book.actions';
 import { Book } from '../models/book';
 
 export const initialState: Book[] = [];
 
 export const BookReducer = createReducer(
   initialState,
-  on(AddBook, (state, { id, title, author }) => [
+  on(AddBook, (state) => {
+    return state;
+  }),
+  on(AddBookSuccess, (state, { id, title, author }) => [
     ...state,
     { id, title, author },
   ]),
-
   on(RemoveBook, (state, { bookId }) =>
     state.filter((book) => book.id !== bookId)
-  )
+  ),
+  on(AddBookFailure, (state, { error }) => {
+    console.log(error);
+    return state;
+  })
 );
